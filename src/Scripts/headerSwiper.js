2b4339929630e6ce1b3b1f9776b70f1dd87fff16
Bahchaev Alexandr <wbahas@mail.ru>
2020-10-19 18:25:51 +0700

    Some fix

---
+++ src/Scripts/headerSwiper.js
@@ -0,0 +1,15 @@
+var headerSwiper = new Swiper('.header__swiper-container', {
+    navigation: {
+        nextEl: '.header__swiper-button-next',
+        prevEl: '.header__swiper-button-prev',
+    },
+    pagination: {
+        el: '.header__swiper-pagination',
+        renderBullet: function (index, className) {
+
+            return '<span class="' + className + '"></span>';
+        },
+        bulletClass: "header__swiper-pagination-bullet",
+        bulletActiveClass: "header__swiper-pagination-bullet-active"
+    }
+});
---
+++ src/Scripts/newsSwiper.js
@@ -0,0 +1,8 @@
+var newsSwiper = new Swiper('.news__swiper-container', {
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    navigation: {
+        nextEl: '.news__swiper-button-next',
+        prevEl: '.news__swiper-button-prev',
+    },
+});
